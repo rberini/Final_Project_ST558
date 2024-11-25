5610b1f2d45e3f4caf0b882680909d74c91fff88
rberini <rmberin2@ncsu.edu>
2024-11-25 13:58:43 -0500

    updated API function

--- API.R
+++ API.R
@@ -1,9 +1,5 @@
 library(tidyverse)
 library(tidymodels)
-library(baguette)
-library(vip)
-library(vetiver)
-library(pins)
 library(plumber)
 
 tidymodels_prefer()
@@ -27,11 +23,60 @@ fmode <- function(x) {
   unique_x[which.max(tabulate(match(x, unique_x)))]
 }
 
-diabetes |>
-  select(all_of(predictors)) |>
-  summarise(across(where(is.factor), ~ fmode(.), .names = "{.col}_mode"),
-            across(where(is.numeric), ~ mean(., na.rm = TRUE), .names = "{.col}_mean")) |>
-  t()
+pred_defaults <-
+  diabetes |>
+  summarise(across(where(is.factor), ~ fmode(.), .names = "{.col}"),
+            across(where(is.numeric), ~ mean(., na.rm = TRUE), .names = "{.col}")) |>
+  as.list()
+pred_defaults
+
+pred_diabetes <- function(HighBP = pred_defaults$HighBP,
+                        HighChol = pred_defaults$HighChol,
+                        BMI = pred_defaults$BMI,
+                        HeartDiseaseorAttack = pred_defaults$HeartDiseaseorAttack,
+                        GenHlth = pred_defaults$GenHlth,
+                        MentHlth = pred_defaults$MentHlth,
+                        PhysHlth = pred_defaults$PhysHlth,
+                        Sex = pred_defaults$Sex,
+                        Age = pred_defaults$Age,
+                        Income = pred_defaults$Income) {
+  pred_tibble <-
+    tibble(
+      Diabetes = pred_defaults$Diabetes,
+      HighBP = HighBP,
+      HighChol = HighChol,
+      CholCheck = pred_defaults$CholCheck,
+      BMI = BMI,
+      Smoker = pred_defaults$Smoker,
+      Stroke = pred_defaults$Stroke,
+      HeartDiseaseorAttack = HeartDiseaseorAttack,
+      PhysActivity = pred_defaults$PhysActivity,
+      Fruits = pred_defaults$Fruits,
+      Veggies = pred_defaults$Veggies,
+      HvyAlcoholConsump = pred_defaults$HvyAlcoholConsump,
+      AnyHealthcare = pred_defaults$AnyHealthcare,
+      NoDocbcCost = pred_defaults$NoDocbcCost,
+      GenHlth = GenHlth,
+      MentHlth = MentHlth,
+      PhysHlth = PhysHlth,
+      DiffWalk = pred_defaults$DiffWalk,
+      Sex = Sex,
+      Age = Age,
+      Education = pred_defaults$Education,
+      Income = Income
+  )
+  
+  pred <-
+    diabetes_rf_final_fit |>
+    extract_workflow() |>
+    predict(pred_tibble) |>
+    bind_cols(pred_tibble) |>
+    select(.pred_class, Diabetes)
+  
+  return(pred)
+}
+
+pred_diabetes()
 
 predictions <-
   diabetes_rf_final_fit |>
