90d9e4819feef53f1cb20f12e1c800117224dcf3
rberini <rmberin2@ncsu.edu>
2024-11-26 07:49:45 -0500

    functioning API

--- API.R
+++ API.R
@@ -10,7 +10,7 @@ load("data/diabetes.RData")
 load("diabetes_rf_final_model.RData")
 load("diabetes_rf_final_fit.RData")
 
-data(diabetes)
+attach(diabetes)
 
 invisible(diabetes_rf_final_model)
 
@@ -25,72 +25,129 @@ fmode <- function(x) {
 
 pred_defaults <-
   diabetes |>
-  summarise(across(where(is.factor), ~ fmode(.), .names = "{.col}"),
+  summarise(across(where(is.factor), ~ as.character(fmode(.), .names = "{.col}")),
             across(where(is.numeric), ~ mean(., na.rm = TRUE), .names = "{.col}")) |>
   as.list()
 pred_defaults
 
-pred_diabetes <- function(HighBP = pred_defaults$HighBP,
-                        HighChol = pred_defaults$HighChol,
-                        BMI = pred_defaults$BMI,
-                        HeartDiseaseorAttack = pred_defaults$HeartDiseaseorAttack,
-                        GenHlth = pred_defaults$GenHlth,
-                        MentHlth = pred_defaults$MentHlth,
-                        PhysHlth = pred_defaults$PhysHlth,
-                        Sex = pred_defaults$Sex,
-                        Age = pred_defaults$Age,
-                        Income = pred_defaults$Income) {
+
+#* Predict diabetes diagnosis
+#* Predicts diabetes diagnosis based on provided inputs or defaults.
+#* <br>
+#* <br>
+#* Override defaults for the following parameters to customize prediction:
+#* - `highbp`: Whether individual has high blood pressure (i.e., "Yes", "No")
+#* - `highchol`: Whether individual has high cholesterol (i.e., "Yes", "No")
+#* - `bmi`: Body Mass Index as a numeric value between 10-100
+#* - `heartdiseaseorattack`: Whether the individual has history of heart disease or attack (i.e., "Yes", "No")
+#* - `genhlth`: General health rating as a numeric value between 1 (excellent) and 5 (poor)
+#* - `menthlth`: Days of poor mental health over last 30 days as a numeric value between 0-30
+#* - `physhlth`: Days of poor physical health over last 30 days as a numeric value between 0-30
+#* - `sex`: Gender (i.e., "Female", "Male")
+#* - `age`: Age group (i.e., "18 to 24", "25 to 29", "30 to 34", "35 to 39", "40 to 44", "45 to 49", "50 to 54", "55 to 59", "60 to 64", "65 to 69", "70 to 74", "75 to 79", "80 or older")
+#* - `income`: Household income group (i.e., "Less than $10,000", "$10,000 to less than $15,000", "$15,000 to less than $20,000", "$20,000 to less than $25,000", "$25,000 to less than $35,000", "$35,000 to less than $50,000", "$50,000 to less than $75,000", "$75,000 or more")
+#* <br>
+#* <br>
+#* __Example calls:__
+#* - `/pred?highbp=Yes&highchol=Yes&bmi=80&heartdiseaseorattack=Yes&genhlth=2&menthlth=2&physhlth=2&sex=Male&age=70%20to%2074&income=%2410%2C000%20to%20less%20than%20%2415%2C000`
+#* - `/pred?highbp=No&highchol=No&bmi=25&heartdiseaseorattack=No&genhlth=1&menthlth=2&physhlth=2&sex=Female&age=25%20to%2029&income=%2450%2C000%20to%20less%20than%20%2475%2C000`
+#* - `/pred?highbp=No&highchol=No&bmi=50&heartdiseaseorattack=No&genhlth=5&menthlth=0&physhlth=0&sex=Male&age=60%20to%2064&income=%2475%2C000%20or%20more`
+#* - `/pred` (uses all default values)
+#* 
+#* @param highbp Yes or No
+#* @param highchol Yes or No
+#* @param bmi:numeric Number between 10-100
+#* @param heartdiseaseorattack Yes or No
+#* @param genhlth:numeric Number between 1-5
+#* @param menthlth:numeric Number between 0-30
+#* @param physhlth:numeric Number between 0-30
+#* @param sex Female or Male
+#* @param age Age group
+#* @param income Income group
+#* 
+#* @get /pred
+pred_diabetes <- function(highbp = pred_defaults$HighBP,
+                        highchol = pred_defaults$HighChol,
+                        bmi = pred_defaults$BMI,
+                        heartdiseaseorattack = pred_defaults$HeartDiseaseorAttack,
+                        genhlth = pred_defaults$GenHlth,
+                        menthlth = pred_defaults$MentHlth,
+                        physhlth = pred_defaults$PhysHlth,
+                        sex = pred_defaults$Sex,
+                        age = pred_defaults$Age,
+                        income = pred_defaults$Income) {
   pred_tibble <-
     tibble(
       Diabetes = pred_defaults$Diabetes,
-      HighBP = HighBP,
-      HighChol = HighChol,
+      HighBP = highbp,
+      HighChol = highchol,
       CholCheck = pred_defaults$CholCheck,
-      BMI = BMI,
+      BMI = as.double(bmi),
       Smoker = pred_defaults$Smoker,
       Stroke = pred_defaults$Stroke,
-      HeartDiseaseorAttack = HeartDiseaseorAttack,
+      HeartDiseaseorAttack = heartdiseaseorattack,
       PhysActivity = pred_defaults$PhysActivity,
       Fruits = pred_defaults$Fruits,
       Veggies = pred_defaults$Veggies,
       HvyAlcoholConsump = pred_defaults$HvyAlcoholConsump,
       AnyHealthcare = pred_defaults$AnyHealthcare,
       NoDocbcCost = pred_defaults$NoDocbcCost,
-      GenHlth = GenHlth,
-      MentHlth = MentHlth,
-      PhysHlth = PhysHlth,
+      GenHlth = as.double(genhlth),
+      MentHlth = as.double(menthlth),
+      PhysHlth = as.double(physhlth),
       DiffWalk = pred_defaults$DiffWalk,
-      Sex = Sex,
-      Age = Age,
+      Sex = sex,
+      Age = age,
       Education = pred_defaults$Education,
-      Income = Income
+      Income = income
   )
   
-  pred <-
+  temp_pred <-
     diabetes_rf_final_fit |>
     extract_workflow() |>
-    predict(pred_tibble) |>
-    bind_cols(pred_tibble) |>
-    select(.pred_class, Diabetes)
+    predict(pred_tibble)
+  
+  temp_prob <-
+    diabetes_rf_final_fit |>
+    extract_workflow() |>
+    predict(pred_tibble, type = "prob")
+  
+  pred <-
+    bind_cols(temp_pred, temp_prob, pred_tibble |> select(-Diabetes))
   
   return(pred)
 }
 
-pred_diabetes()
 
-predictions <-
-  diabetes_rf_final_fit |>
-  extract_workflow() |>
-  predict(diabetes) |>
-  bind_cols(diabetes) |>
-  select(.pred_class, Diabetes)
-predictions
 
-accuracy_metric <-
-  predictions |> 
-  metrics(truth = Diabetes, estimate = .pred_class) |>
-  filter(.metric == "accuracy")
-accuracy_metric
 
-conf_matrix <-conf_mat(predictions, truth = Diabetes, estimate = .pred_class)
-conf_matrix
+#* Return name and final project website
+#* @serializer html
+#* @get /info
+function() {
+  "<!DOCTYPE html>
+  <html>
+  <head><title>Plumber HTML</title></head>
+  <body>
+    <h4>Rob Berini</h4>
+    <a href='https://rberini.github.io/Final_Project_ST558/'>Final Project Website</a>
+  </body>
+  </html>"
+}
+
+
+#* Get confusion matrix for entire data set
+#* @serializer print
+#* @get /confusion
+function() {
+  predictions <-
+    diabetes_rf_final_fit |>
+    extract_workflow() |>
+    predict(diabetes) |>
+    bind_cols(diabetes) |>
+    select(.pred_class, Diabetes)
+  
+  conf_matrix <-conf_mat(predictions, truth = Diabetes, estimate = .pred_class)
+  conf_matrix
+  }
+
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Final_Project_ST558
+
+## Rob Berini
+
+[EDA Webpage](https://rberini.github.io/Final_Project_ST558/EDA.html "EDA")
+
+[Modeling Webpage](https://rberini.github.io/Final_Project_ST558/Modeling.html "Modeling")
+
+[Diabetes Dataset](https://www.kaggle.com/datasets/alexteboul/diabetes-health-indicators-dataset/?select=diabetes_binary_health_indicators_BRFSS2015.csv "Diabetes Dataset")
+
+[Codebook for Variables](https://www.cdc.gov/brfss/annual_data/2014/pdf/CODEBOOK14_LLCP.pdf "Codebook")
+
+|  |  |  |  |  |
+|---------------|---------------|---------------|---------------|---------------|
+| Diabetes | Target | Binary | Factor | 0 = no diabetes 1 = prediabetes or diabetes |
+| HighBP | Feature | Binary | Factor | 0 = no high BP 1 = high BP |
+| HighChol | Feature | Binary | Factor | 0 = no high cholesterol 1 = high cholesterol |
+| CholCheck | Feature | Binary | Factor | 0 = no cholesterol check in 5 years 1 = yes cholesterol check in 5 years |
+| BMI | Feature | Integer | Double | Body Mass Index |
+| Smoker | Feature | Binary | Factor | Have you smoked at least 100 cigarettes in your entire life? \[Note: 5 packs = 100 cigarettes\] 0 = no 1 = yes |
+| Stroke | Feature | Binary | Factor | (Ever told) you had a stroke. 0 = no 1 = yes |
+| HeartDiseaseorAttack | Feature | Binary | Factor | coronary heart disease (CHD) or myocardial infarction (MI) 0 = no 1 = yes |
+| PhysActivity | Feature | Binary | Factor | physical activity in past 30 days - not including job 0 = no 1 = yes |
+| Fruits | Feature | Binary | Factor | Consume Fruit 1 or more times per day 0 = no 1 = yes |
+| Veggies | Feature | Binary | Factor | Consume Vegetables 1 or more times per day 0 = no 1 = yes |
+| HvyAlcoholConsump | Feature | Binary | Factor | Heavy drinkers (adult men having more than 14 drinks per week and adult women having more than 7 drinks per week) 0 = no 1 = yes |
+| AnyHealthcare | Feature | Binary | Factor | Have any kind of health care coverage, including health insurance, prepaid plans such as HMO, etc. 0 = no 1 = yes |
+| NoDocbcCost | Feature | Binary | Factor | Was there a time in the past 12 months when you needed to see a doctor but could not because of cost? 0 = no 1 = yes |
+| GenHlth | Feature | Integer | Double | Would you say that in general your health is: scale 1-5 1 = excellent 2 = very good 3 = good 4 = fair 5 = poor |
+| MentHlth | Feature | Integer | Double | Now thinking about your mental health, which includes stress, depression, and problems with emotions, for how many days during the past 30 days was your mental health not good? scale 1-30 days |
+| PhysHlth | Feature | Integer | Double | Now thinking about your physical health, which includes physical illness and injury, for how many days during the past 30 days was your physical health not good? scale 1-30 days |
+| DiffWalk | Feature | Binary | Factor | Do you have serious difficulty walking or climbing stairs? 0 = no 1 = yes |
+| Sex | Feature | Binary | Facor | 0 = female 1 = male |
+| Age | Feature | Integer | Ordered Factor | 13-level age category (\_AGEG5YR see codebook) 1 = 18-24 9 = 60-64 13 = 80 or older |
+| Education | Feature | Integer | Ordered Factor | Education level (EDUCA see codebook) scale 1-6 1 = Never attended school or only kindergarten 2 = Grades 1 through 8 (Elementary) 3 = Grades 9 through 11 (Some high school) 4 = Grade 12 or GED (High school graduate) 5 = College 1 year to 3 years (Some college or technical school) 6 = College 4 years or more (College graduate) |
+| Income | Feature | Integer | Ordered Factor | Income scale (INCOME2 see codebook) scale 1-8 1 = less than \$10,000 5 = less than \$35,000 8 = \$75,000 or more |
